3e62be7a0bd06351258bd9d94ddaed6c52f342cb
Denize Patric Bautista Ignacio <denizeignacio@gmail.com>
2019-07-08 21:17:15 -0700

    User is able to login to account.

--- app/src/main/java/com/example/instagramapp/MainActivity.java
+++ app/src/main/java/com/example/instagramapp/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.instagramapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etUsernameInput;
+    private EditText etPassword;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // connecting text view and buttons to main activity
+        etUsernameInput = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // extracting username and password data from user
+                final String username = etUsernameInput.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                // passing in username and password data to login method
+                login(username, password);
+            }
+        });
+    }
+
+    // method to login user
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e == null) {
+                    Log.d("LoginActivity", "Login successful");
+                    final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Log.e("LoginActivity", "Login Failure");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
